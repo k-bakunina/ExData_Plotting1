0c61c5a9d8278ae483de2d170a32dd5d243b76eb
k-bakunina <k_bakunina@yahoo.com>
2014-05-12 01:47:38 +0300

    I did it! :)

---
+++ plot1.R
@@ -0,0 +1,5 @@
+data<-read.table("household_power_consumption.txt", header=TRUE, sep = ";", nrows = 2880, skip = 66636)
+names(data)<-c("Date","Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot1.png")
+dev.off()
